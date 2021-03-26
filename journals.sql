5a605d62e6868f930499221eaa1d26297bf93f99
Jacob Hilker <jacob.hilker2@gmail.com>
2021-03-25 20:02:27 -0400

    update sql template

--- journals.sql
+++ journals.sql
@@ -1,3 +1,28 @@
-CREATE DATABASE journals;
+CREATE DATABASE journals,
 \c journals
 
+create table users 
+/*(
+    primary key,
+    // username string,
+    // email string,
+    // password 
+); */
+create table journals
+/* (
+    foreign key,
+    Entry entry
+
+);
+*/
+
+create table journalEntries (
+    /* foreign key,*/
+    publicity boolean,
+    date_created date,
+    entry string,
+    last_modified date,
+    user_id int,
+    activity_level int,
+    published boolean;
+)
